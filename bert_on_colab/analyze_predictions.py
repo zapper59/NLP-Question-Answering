0d295f07c53bc6031a601695608b77ca4ba68777
flippedaben <ben.yang@utexas.edu>
2019-04-28 16:19:39 -0500

    Update predictions analyzing for v2

--- bert_on_colab/analyze_predictions.py
+++ bert_on_colab/analyze_predictions.py
@@ -7,23 +7,17 @@ parser = argparse.ArgumentParser()
 parser.add_argument("preds", help="path to predictions json file")
 args = parser.parse_args()
 
-with open(args.preds) as f:
+:
+### Get answer
+with open(args.preds, 'r') as f:
     preds = json.load(f)
 
 freq = collections.Counter()
-sumscores = {}
+
 for i in preds:
-    ans = preds[i]['text']
-    score = preds[i]['probability']
-    freq[ans] += 1
-    if ans in sumscores:
-        sumscores[ans] += score
-    else:
-        sumscores[ans] = score
+    ans = preds[i][0]['text']
+    if ans:
+        freq[ans] += 1
 
-avgscores = {}
-for text, c in freq.most_common(3):
-    avgscores[text] = sumscores[text]/c
-
-top_avgscores = sorted(avgscores.items(), key=operator.itemgetter(1), reverse=True)
-print(top_avgscores)
+with open('predictions.txt', 'a') as f:
+    f.write(freq.most_common(1)[0][0])
