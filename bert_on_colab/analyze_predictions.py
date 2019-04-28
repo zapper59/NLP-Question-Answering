c2d8500d7e99af42902a95cf98ad4641976e96f0
flippedaben <ben.yang@utexas.edu>
2019-04-28 17:22:22 -0500

    Change a.p.

--- bert_on_colab/analyze_predictions.py
+++ bert_on_colab/analyze_predictions.py
@@ -12,11 +12,21 @@ with open(args.preds, 'r') as f:
     preds = json.load(f)
 
 freq = collections.Counter()
+m = {}
 
 for i in preds:
     ans = preds[i][0]['text']
+    score = preds[i][0]['probability']
     if ans:
         freq[ans] += 1
+        if ans in m:
+            m[ans] = max(m[ans], score)
+        else:
+            m[ans] = score
 
-with open('predictions.txt', 'a') as f:
-    f.write(freq.most_common(1)[0][0] + '\n')
+topm = sorted(maxscores.items(), key=operator.itemgetter(1), reverse=True)
+print(topm[0:3])
+print(freq.most_common(3))
+
+# with open('predictions.txt', 'a') as f:
+#     f.write(freq.most_common(1)[0][0] + '\n')
